bf99cb2b8f2043b7f025ec8c02bc4f4b6721af77
Isa <pdo.isabelle@gmail.com>
2024-03-30 10:01:22 -0300

    Feat: Criação de rota para atualização de localização

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,30 @@
 class UsersController < ApplicationController
-  def create
-    user = User.new(user_params)
+	def create
+		user = User.new(user_params)
 
-    if user.save
-      render json: user, status: :created
-    else
-      render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
-    end
-  end
+		if user.save
+			render json: user, status: :created
+		else
+			render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+		end
+	end
 
-  private
+	def update_location
+		user = User.find(params[:id])
+		if user.update(user_location_params)
+			render json: user, status: :ok
+		else
+			render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+		end
+	end
 
-  def user_params
-    params.require(:user).permit(:name, :age, :gender, :latitude, :longitude, :health, :username, :encrypted_password)
-  end
+	private
+
+	def user_params
+		params.require(:user).permit(:name, :age, :gender, :latitude, :longitude, :health, :username, :encrypted_password)
+	end
+
+	def user_location_params
+		params.require(:user).permit(:latitude, :longitude)
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { registrations: 'registrations' }
+	devise_for :users, controllers: { registrations: 'registrations' }
 
-  # Route for creating users
-  resources :users, only: [:create]
+	# Route for creating users
+	#resources :users, only: [:create]
+
+	# Route for users
+	resources :users do
+		member do
+			post 'create'
+			get 'update_location'
+		end
+	end
 end
