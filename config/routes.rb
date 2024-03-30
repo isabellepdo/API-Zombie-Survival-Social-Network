aa20dd66ca67021b0c15bb6d964429691e972a9a
Isa <pdo.isabelle@gmail.com>
2024-03-30 09:04:00 -0300

    Style: mudança de texto para inglês

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'registrations' }
 
-  # Rota para criação de usuários
+  # Route for creating users
   resources :users, only: [:create]
 end
