6504ae4d0d8d806a4197647b947934e1609b821a
Isa <pdo.isabelle@gmail.com>
2024-04-02 08:33:18 -0300

    Teste: Criação de testes para escambo

--- spec/controllers/inventory_movement_controller_spec.rb
+++ spec/controllers/inventory_movement_controller_spec.rb
@@ -62,31 +62,155 @@ RSpec.describe InventoryMovementController, type: :controller do
 			end
 
 			it 'updates the inventory with the correct amount' do
-				user_inventory = Inventory.where(user_id: User.find_by_username('teste2').id, item_id: Item.find_by_name('Água').id).last
+				user = User.find_by_username('teste2')
+				item = Item.find_by_name('Água')
+				user_inventory = Inventory.where(user_id: user.id, item_id: item.id).last
 				user_inventory.update_column(:amount, 3)
-				byebug
+
 				get :remove_item, params: valid_params, format: :json
-				expect(user_inventory.amount).to eq(2)
+				expect(user.inventories.where(item_id: item.id).last.amount).to eq(2)
 			end
 		end
 
-		# context 'with invalid JSON parameters' do
-		# 	let(:invalid_params) do
-		# 		{
-		# 			user: 'non_existing_user',
-		# 			item: 'item_name',
-		# 			value: 1
-		# 		}
-		# 	end
+		context 'with invalid JSON parameters' do
+			let(:invalid_params) do
+				{
+					user: 'non_existing_user',
+					item: 'item_name',
+					value: 1
+				}
+			end
 
-		# 	it 'returns an error if user is not found' do
-		# 		post :remove_item, params: invalid_params, format: :json
-		# 		expect(response).to have_http_status(:not_found)
-		# 	end
-		# end
+			it 'returns an error if user is not found' do
+				post :remove_item, params: invalid_params, format: :json
+				expect(response).to have_http_status(:not_found)
+			end
+		end
 	end
 
-	# describe 'POST #barter' do
-	# 	# Write your test cases for the barter action
-	# end
+	describe 'POST #barter' do
+		context 'with valid JSON parameters and valid users/items' do
+			let!(:item1) { Item.create(name: 'Item1', points_for_barter: 4) }
+			let!(:item2) { Item.create(name: 'Item2', points_for_barter: 5) }
+			let!(:user1) { User.create(name: 'User1', username: 'user1', encrypted_password: 'password') }
+			let!(:user2) { User.create(name: 'User2', username: 'user2', encrypted_password: 'password') }
+			let!(:inventory_user1_item1) { Inventory.create(user_id: user1.id, item_id: item1.id, amount: 3) }
+			let!(:inventory_user1_item2) { Inventory.create(user_id: user1.id, item_id: item2.id, amount: 2) }
+			let!(:inventory_user2_item1) { Inventory.create(user_id: user2.id, item_id: item1.id, amount: 2) }
+			let!(:inventory_user2_item2) { Inventory.create(user_id: user2.id, item_id: item2.id, amount: 3) }
+			let(:valid_barter_params) do
+				{
+					users: [
+						{
+							username: 'user1',
+							items: [
+								{ name: 'Item1', amount: 1 },
+								{ name: 'Item2', amount: 1 }
+							]
+						},
+						{
+							username: 'user2',
+							items: [
+								{ name: 'Item1', amount: 1 },
+								{ name: 'Item2', amount: 1 }
+							]
+						}
+					]
+				}
+			end
+	
+			it 'successfully performs the barter between users' do
+				post :barter, body: valid_barter_params.to_json, format: :json
+				expect(response).to have_http_status(:ok)
+			end
+	
+			it 'updates the inventories with the correct amounts after the barter' do
+				post :barter, body: valid_barter_params.to_json, format: :json
+	
+				expect(user1.inventories.find_by(item_id: item1.id).amount).to eq(2)
+				expect(user1.inventories.find_by(item_id: item2.id).amount).to eq(1)
+				expect(user2.inventories.find_by(item_id: item1.id).amount).to eq(3)
+				expect(user2.inventories.find_by(item_id: item2.id).amount).to eq(4)
+			end
+		end
+	
+		context 'with invalid JSON parameters' do
+			let(:invalid_barter_params) do
+				{
+					invalid_key: 'value'
+				}
+			end
+	
+			it 'returns an error if JSON structure is invalid' do
+				post :barter, body: invalid_barter_params.to_json, format: :json
+				expect(response).to have_http_status(:unprocessable_entity)
+			end
+		end
+	
+		context 'when one or both users are not found' do
+			let(:invalid_barter_params) do
+				{
+					users: [
+						{
+							username: 'non_existing_user',
+							items: [{ name: 'Item1', amount: 1 }]
+						},
+						{
+							username: 'user2',
+							items: [{ name: 'Item2', amount: 1 }]
+						}
+					]
+				}
+			end
+	
+			it 'returns an error if one or both users are not found' do
+				post :barter, body: invalid_barter_params.to_json, format: :json
+				expect(response).to have_http_status(:not_found)
+			end
+		end
+	
+		context 'when one or both items are not found' do
+			let(:invalid_barter_params) do
+				{
+					users: [
+						{
+							username: 'user1',
+							items: [{ name: 'NonExistingItem', amount: 1 }]
+						},
+						{
+							username: 'user2',
+							items: [{ name: 'Item2', amount: 1 }]
+						}
+					]
+				}
+			end
+	
+			it 'returns an error if one or both items are not found' do
+				post :barter, body: invalid_barter_params.to_json, format: :json
+				expect(response).to have_http_status(:not_found)
+			end
+		end
+	
+		context 'when exchange does not satisfy points rules' do
+			let(:invalid_barter_params) do
+				{
+					users: [
+						{
+							username: 'user1',
+							items: [{ name: 'Item1', amount: 1 }]
+						},
+						{
+							username: 'user2',
+							items: [{ name: 'Item2', amount: 1 }]
+						}
+					]
+				}
+			end
+	
+			it 'returns an error if exchange does not satisfy points rules' do
+				post :barter, body: invalid_barter_params.to_json, format: :json
+				expect(response).to have_http_status(:unprocessable_entity)
+			end
+		end
+	end	
 end
